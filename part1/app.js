2e01eb0f88431a0c5e7547db2ec414aeaa64a423
Taylor Dziego <taylor.dziego@student.tafesa.edu.au>
2025-06-20 04:27:05 +0000

    [automatic commit] 2025-06-20 04:27:04 UTC 116.91.223.146

--- part1/app.js
+++ part1/app.js
@@ -44,7 +44,8 @@ app.get('/api/walkers/summary', async (req, res) => {
             ROUND(AVG(r.rating), 1) AS avergae_rating,
             COUNT (DISTINCT wr.request_id) AS completed_walks
             FROM Users u
-            LEFT JOIN WalkRatings r ON u:user_id = r.walker_id`
+            LEFT JOIN WalkRatings r ON u:user_id = r.walker_id
+            LEFT JOIN WalkRequests wr ON wr.status`
         )
     }
 })
