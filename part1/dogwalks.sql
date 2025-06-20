290ee5c1a5abc791b5abcf9948cad3c412edd6ff
Taylor Dziego <taylor.dziego@student.tafesa.edu.au>
2025-06-20 03:46:26 +0000

    [automatic commit] 2025-06-20 03:46:26 UTC 116.91.223.146

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -70,3 +70,7 @@ VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'Medium'
 
 INSERT INTO WalkRequest (dog_id, requested_time, duration_minutes, location, status)
 VALUES ((SELECT dog_id FROM Dogs WHERE name =''),'', ,'',''),
+VALUES ((SELECT dog_id FROM Dogs WHERE name =''),'', ,'',''),
+VALUES ((SELECT dog_id FROM Dogs WHERE name =''),'', ,'',''),
+VALUES ((SELECT dog_id FROM Dogs WHERE name =''),'', ,'',''),
+VALUES ((SELECT dog_id FROM Dogs WHERE name =''),'', ,'','');
