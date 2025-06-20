454e7b8645f71f0653c4534cfaa26751d86cd54b
Taylor Dziego <taylor.dziego@student.tafesa.edu.au>
2025-06-20 04:20:44 +0000

    [automatic commit] 2025-06-20 04:20:44 UTC 116.91.223.146

--- part1/app.js
+++ part1/app.js
@@ -32,6 +32,6 @@ app.get('/api/dogs', async (req, res) => {
         );
         res.json(rows);
     } catch (err) {
-        res.status(500).json({ error})
+        res.status(500).json({ error: err.messsage });
     }
 });
