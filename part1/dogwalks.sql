67ed6c7a5dd63b6cb14f9e03e70af0dae22799d3
Taylor Dziego <taylor.dziego@student.tafesa.edu.au>
2025-06-20 03:42:01 +0000

    [automatic commit] 2025-06-20 03:42:01 UTC 116.91.223.146

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -63,4 +63,7 @@ VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 
 INSERT INTO Dogs (owner_id, name, size)
 VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'Medium'),
-()
+((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'Medium'),
+((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'Medium'),
+((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'Medium'),
+((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'Medium');
