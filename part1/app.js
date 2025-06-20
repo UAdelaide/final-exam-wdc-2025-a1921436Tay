f720a37e72ff43bc1cf5e9e6bd1dee78dd4417b4
Taylor Dziego <taylor.dziego@student.tafesa.edu.au>
2025-06-20 04:36:27 +0000

    [automatic commit] 2025-06-20 04:36:27 UTC 116.91.223.146

--- part1/app.js
+++ part1/app.js
@@ -44,8 +44,8 @@ app.get('/api/walkrequests/open', async (req, res) => {
             FROM WalkRequests wr
             JOIN Dogs d ON wr.dog_id = d.dog_id
             JOIN Users u ON d.owner_id = u.user_id
-            WHERE wr.status`
-        )
+            WHERE wr.status = 'open'`
+        );
     }
 })
 app.get('/api/walkers/summary', async (req, res) => {
