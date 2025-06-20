3ebd9b5a80144d54cace074fe935e27c157d5931
Taylor Dziego <taylor.dziego@student.tafesa.edu.au>
2025-06-20 04:18:22 +0000

    [automatic commit] 2025-06-20 04:18:22 UTC 116.91.223.146

--- part1/app.js
+++ part1/app.js
@@ -26,7 +26,7 @@ start();
 app.get('/api/dogs', async (req, res) => {
     try {
         const [rows] = await db.execute(
-            SELECT d.name AS dog_name, d.size
+            SELECT d.name AS dog_name, d.size, u.username As owner_username
         )
     }
 });
