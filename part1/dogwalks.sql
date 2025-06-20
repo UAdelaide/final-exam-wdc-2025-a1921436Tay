35d7d80ce147289f118a0b97114795b88912efba
Taylor Dziego <taylor.dziego@student.tafesa.edu.au>
2025-06-20 03:49:49 +0000

    [automatic commit] 2025-06-20 03:49:49 UTC 116.91.223.146

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -71,6 +71,6 @@ VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'Medium'
 INSERT INTO WalkRequest (dog_id, requested_time, duration_minutes, location, status)
 VALUES ((SELECT dog_id FROM Dogs WHERE name ='Max'),'2025-06-10 08:00:00', 30,'Parklands','open'),
 VALUES ((SELECT dog_id FROM Dogs WHERE name ='Bella'),'2025-06-10 09:30:00', 45,'Beachside Ave','accepted'),
-VALUES ((SELECT dog_id FROM Dogs WHERE name ='Tiny'),'2025-06-11 10:45:00', ,'',''),
+VALUES ((SELECT dog_id FROM Dogs WHERE name ='Tiny'),'2025-06-11 10:45:00', 30,'',''),
 VALUES ((SELECT dog_id FROM Dogs WHERE name ='Biggie'),'', ,'',''),
 VALUES ((SELECT dog_id FROM Dogs WHERE name ='Spot'),'', ,'','');
