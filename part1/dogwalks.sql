260a463c45dd7bbc6379d82d71ff6c6536e84268
Taylor Dziego <taylor.dziego@student.tafesa.edu.au>
2025-06-20 03:38:57 +0000

    [automatic commit] 2025-06-20 03:38:57 UTC 116.91.223.146

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -58,4 +58,5 @@ INSERT INTO Users (username, email, password_hash, role)
 VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('fredwalker', 'fred@example.com', 'hashed111', 'walker')
+('fredwalker', 'fred@example.com', 'hashed111', 'walker'),
+('pam123', )
