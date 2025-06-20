a11aa787e43d98ef4aa82e9382a18fb263e25617
Taylor Dziego <taylor.dziego@student.tafesa.edu.au>
2025-06-20 03:35:52 +0000

    [automatic commit] 2025-06-20 03:35:52 UTC 116.91.223.146

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -55,5 +55,5 @@ CREATE TABLE WalkRatings (
 );
 
 INSERT INTO Users (username, email, password_hash, role) VALUES (
-    'alice123', alice@example.com'
+    'alice123', 'alice@example.com', 'hashed123',
 )
