2992a45bd72997f7be5519185a68bb814d952b5b
Taylor Dziego <taylor.dziego@student.tafesa.edu.au>
2025-06-21 06:01:52 +0000

    [automatic commit] 2025-06-21 06:01:52 UTC 116.91.221.125

--- part2/app.js
+++ part2/app.js
@@ -4,15 +4,6 @@ require('dotenv').config();
 
 const app = express();
 
-//session middleware
-app.use(session({
-  secret: 'mySuperSecret',
-  resave: false,
-  saveUninitialized: false,
-  cookie: { secure: false } // false for local HTTP
-}));
-
-
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
