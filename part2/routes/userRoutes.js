ebe7b799754355a2737f84c36294e1994fc9661e
Taylor Dziego <taylor.dziego@student.tafesa.edu.au>
2025-06-21 05:29:45 +0000

    [automatic commit] 2025-06-21 05:29:44 UTC 116.91.221.125

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -50,7 +50,6 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
     req.session.user = rows[0];
-
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
@@ -74,15 +73,12 @@ router.post('/logout', (req, res) => {
 // api dogs owned
 router.get('/dogs/owned', async (req, res) => {
   if (!req.session.user || req.session.user.role !== 'owner') {
-    return res.status(403).json({ error: 'Access denied' });
+    return res.status(403).json({ error: 'Denied' });
   }
-  try {
-    const [rows] = await db.query(
-      'SELECT dog_id, name FROM Dogs WHERE owner_id = ?',
-      [req.session.user.user_id]
-    );
-    res.json(rows);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch dogs' });
-  }
+
+  const [rows] = await db.query(
+    'SELECT dog_id, name FROM Dogs WHERE owner_id = ?',
+    [req.session.user.user_id]
+  );
+  res.json(rows);
 });
