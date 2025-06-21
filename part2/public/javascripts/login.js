d0511bfa8cea8bdfe9f86e1a14516920000c041e
Taylor Dziego <taylor.dziego@student.tafesa.edu.au>
2025-06-21 04:36:26 +0000

    [automatic commit] 2025-06-21 04:36:25 UTC 116.91.221.125

--- part2/public/javascripts/login.js
+++ part2/public/javascripts/login.js
@@ -39,20 +39,4 @@ createApp({
 }).mount('#app');
 
 
-// LogOut Functionaility
 
-document.getElementById('logout-button').addEventListener('click', async () => {
-    try {
-        const res = await fetch('/api/users/logout', {
-            method: 'POST',
-            credentials: 'include'
-        });
-        if (res.ok) {
-            window.location.href = '/';
-        } else {
-            error.value = 'Error, Logout Failed';
-        }
-    } catch (err) {
-        error.value = 'An error occurred during logout.';
-    }
-});
